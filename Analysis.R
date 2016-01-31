dc4fd714cf9edf95a20fb962882013da964d63a0
makendi1984 <makendiamos@yahoo.com>
2016-01-31 23:15:55 +0100

    Update Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -12,27 +12,27 @@ features <- read.table("UCI HAR Dataset/features.txt")
 features[,2] <- as.character(features[,2])
 
 # Extract only the data on mean and standard deviation
-featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
-featuresWanted.names <- features[featuresWanted,2]
-featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
-featuresWanted.names = gsub('-std', 'Std', featuresWanted.names)
-featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
+FW <- grep(".*mean.*|.*std.*", features[,2])
+FW.names <- features[FW,2]
+FW.names = gsub('-mean', 'Mean', FW.names)
+FW.names = gsub('-std', 'Std', FW.names)
+FW.names <- gsub('[-()]', '', FW.names)
 
 
 # Loading respective datasets
-train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[FW]
 Activities <- read.table("UCI HAR Dataset/train/Y_train.txt")
 Subjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
 train <- cbind(Subjects, Activities, train)
 
-test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresWanted]
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[FW]
 Activities <- read.table("UCI HAR Dataset/test/Y_test.txt")
 Subjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
 test <- cbind(Subjects, Activities, test)
 
 # merge datasets and add labels
 wholeData <- rbind(train, test)
-colnames(wholeData) <- c("subject", "activity", featuresWanted.names)
+colnames(wholeData) <- c("subject", "activity", FW.names)
 
 # turn activities & subjects into factors
 wholeData$activity <- factor(wholeData$activity, levels = Labelsactivity[,1], labels = Labelsactivity[,2])
@@ -41,4 +41,4 @@ wholeData$subject <- as.factor(wholeData$subject)
 wholeData.melted <- melt(wholeData, id = c("subject", "activity"))
 wholeData.mean <- dcast(wholeData.melted, subject + activity ~ variable, mean)
 
-write.table(wholeData.mean, "tidydata.txt", row.names = FALSE, quote = FALSE)
+write.table(wholeData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
